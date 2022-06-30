173d7fe9f45e44ba0a4ba8b18f7b16040b7e7f6f
Paras Sharma <parasangiras121@gmail.com>
2022-06-30 21:48:26 +0530

    conditioning

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import pic2 from "../src/images/ig.png";
 import pic3 from "../src/images/tw.png";
 
 var userlogin = false;
+var userIsregistered = true;
 
 function check(userlogin){
     if(userlogin===true){
@@ -14,17 +15,22 @@ function check(userlogin){
     else{
         return(
             <div className="login-box">
-                <h1>Please Login!</h1>
+                <h1>
+                    {userIsregistered?"Please Login":"Please Register!"}
+                </h1>
                 <div className="input-container">
                     <input placeholder="Username" type="text"></input>
                     <input placeholder="Password" type="password"></input>
+                    {userIsregistered?null:<input placeholder="Confirm Password" type="password"></input>}
                 </div>
                 <div className="social">
                     <img src={pic1} alt="fb"/>
                     <img src={pic2} alt="ig"/>
                     <img src={pic3} alt="tw"/>
                 </div>
-                <button>Login</button>
+                <button>
+                    {userIsregistered?"Login":"Register"}
+                </button>
             </div>
         )
     }
